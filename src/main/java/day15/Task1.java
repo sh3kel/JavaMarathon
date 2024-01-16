48ef4a312293d8ec2f9d0d650f0963ad263b1fb0
sh3kel <sh3kel.a@yandex.ru>
2024-01-16 20:22:17 +0300

    fixed day15

--- src/main/java/day15/Task1.java
+++ src/main/java/day15/Task1.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.text.NumberFormat;
 import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.List;
@@ -22,29 +23,26 @@ public class Task1 {
 
 //Функция фильтрации данных. Принимает на вход файл для чтения и файл для записи.
     public static void filterData(File readFile, File writeFile) {
-        try {
-            Scanner scan = new Scanner(readFile); //инициализируем сканнер из файла для чтения
-            PrintWriter fileWriter = new PrintWriter(writeFile); //инициализируем объект для записи в файл
+        try(Scanner scan = new Scanner(readFile);
+            PrintWriter fileWriter = new PrintWriter(writeFile))
+        {
             int count; //переменная для проверки кол-ва товара
             while (scan.hasNextLine()) {
                 String[] data = scan.nextLine().split(";"); //разделяем строку с помощью метода split
                 if (data.length != 3) { //проверяем, что количество столбцов равно 3
                     throw new IOException();
                 }
-                try {
-                    count = Integer.parseInt(data[2]);
-                    if (count == 0) {
-                        fileWriter.println(data[0] + ", " + data[1] + ", " + data[2]); //Если кол-во товара 0, записываем в файл
-                    }
-                } catch (NumberFormatException e) { //если кол-во товара невозможно преобразовать в формат int
-                    System.out.println("Некорректно указано количество товара.");
+                count = Integer.parseInt(data[2]);
+                if (count == 0) {
+                    fileWriter.println(data[0] + ", " + data[1] + ", " + data[2]); //Если кол-во товара 0, записываем в файл
                 }
             }
-            fileWriter.close(); //закрываем поток
         } catch (FileNotFoundException e) { //если не удается найти файл
             System.out.println("Файл не найден.");
         } catch (IOException e) { //если кол-во столбцов не равно 3
             System.out.println("Некоррентный формат данных");
+        } catch (NumberFormatException e) {
+            System.out.println("Некорректно указано количество товара.");
         }
     }
 }
