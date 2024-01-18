c544b5d179760519d6824a2d58b95ce7aa7f9d11
sh3kel <sh3kel.a@yandex.ru>
2024-01-18 16:54:08 +0300

    fixed day19

--- src/main/java/day19/Task1.java
+++ src/main/java/day19/Task1.java
@@ -1,10 +1,61 @@
 package day19;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
+
 /**
  * @author Neil Alishev
  */
 public class Task1 {
     public static void main(String[] args) {
+        //Сканируем файл
+        String sep = File.separator;
+        String path = "src" + sep + "main" + sep + "resources" + sep + "dushi.txt";
+        File text = new File(path);
+        //Создаем words - объект типа hashMap для хранения слов в кач-ве ключа и кол-ва их упоминаний в кач-ве значения
+        Map<String, Integer> words = new HashMap<>();
 
+        try {
+            //Объект типа scan, принимающий на вход файл
+            Scanner scan = new Scanner(text);
+            scan.useDelimiter("[.,:;()?!\"\\s–]+");
+            //В цикле проходимся по всем словам в тексте
+            while(scan.hasNext()) {
+                //Создаем переменную word - текущее слово в нижнем регистре
+                String word = scan.next().toLowerCase();
+                //Если слово уже есть в words, увеличиваем value на 1
+                if (words.containsKey(word)) {
+                    words.replace(word, words.get(word) + 1);
+                //Иначе добавляем слово в words со значением 1
+                } else {
+                    words.put(word, 1);
+                }
+            }
+        } catch (FileNotFoundException e) {
+            System.out.print("Файл не найден");
+        }
+        //Инициализируем цикл в промежутке [0;100)
+        for(int i = 0; i < 100; i++) {
+            //Инициализируем объект типа Entry wordToOut, который будет словом для вывода на экран
+            Map.Entry<String, Integer> wordToOut = null;
+            //Во вложенном цикле проходимся по всем элементам words
+            for (Map.Entry<String, Integer> entry : words.entrySet()) {
+                //Если wordToOut == null, приравниваем его к первому элементу из words
+                if(wordToOut == null) {
+                    wordToOut = entry;
+                }
+                //Если значение wordToOut меньше чем значение текущего элемента words в цикле, то прирваниваем wordToOut
+                //к этому элементу.
+                if (wordToOut.getValue() < entry.getValue()) {
+                    wordToOut = entry;
+                }
+            }
+            //Выводим на экран слово с максимальным количеством упоминаний
+            System.out.println(wordToOut);
+            //Удаляем это слово из words
+            words.remove(wordToOut.getKey());
+            //ОТВЕТ: КОЛИЧЕСТВО УПОМИНАНИЙ "ЧИЧИКОВ" = 601
+        }
     }
 }
--- src/main/java/day19/task2/Task2.java
+++ src/main/java/day19/task2/Task2.java
@@ -1,11 +1,62 @@
 package day19.task2;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
+
 /**
  * @author Neil Alishev
  */
 public class Task2 {
     public static void main(String[] args) {
+        //Создаем объект класса File
+        String sep = File.separator;
+        String path = "src" + sep + "main" + sep + "resources" + sep + "taxi_cars.txt";
+        File file = new File(path);
 
+        //Создаем объект, хранящий в себе информацию о машинах
+        Map<Integer, Point> taxiCars = new HashMap<>();
+
+        try (Scanner scan = new Scanner(file)) {
+            //В цикле считываем информацию о каждой машине
+            while(scan.hasNextInt()) {
+                taxiCars.put(scan.nextInt(), new Point(scan.nextInt(), scan.nextInt()));
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("Файл не найден");
+        }
+
+        //Просим ввести с клавиатуры координаты точек
+        System.out.println("Введите координаты x1, y1, x2, y2");
+        Scanner coords = new Scanner(System.in);
+
+        int x1 = coords.nextInt();
+        int y1 = coords.nextInt();
+        int x2 = coords.nextInt();
+        int y2 = coords.nextInt();
+
+        //Счетчик кол-ва машин, входящих в заданную область
+        int count = 0;
+
+        //В цикле foreach проходимся по taxiCars
+        for(Map.Entry<Integer, Point> entry: taxiCars.entrySet()) {
+            //Если проверка выполняется успешно, выводим на экран идентификатор авто и инкрементируем count
+            if(pointCheck(x1, y1, x2, y2, entry.getValue().getX(), entry.getValue().getY())) {
+                System.out.print(entry.getKey() + " ");
+                count++;
+            }
+        }
+        System.out.print("\nКоличество машин в заданной области: " + count);
+    }
+
+    //Метод для проверки, лежит ли точка в заданной области. Принимает на вход координаты плоскости и точки
+    public static boolean pointCheck(int x1, int y1, int x2, int y2, int x, int y) {
+        if(x > x1 && x < x2 && y > y1 && y < y2 ) {
+            return true;
+        } else if (x < x1 && x > x2 && y < y1 && y > y2) {
+            return true;
+        }
+        return false;
     }
 }
 
