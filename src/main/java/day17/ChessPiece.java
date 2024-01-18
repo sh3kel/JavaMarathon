020fc0307007a4f1d7a19edbeb103457dca41369
sh3kel <sh3kel.a@yandex.ru>
2024-01-18 11:41:22 +0300

    fixed day17

--- src/main/java/day17/ChessPiece.java
+++ src/main/java/day17/ChessPiece.java
@@ -4,20 +4,20 @@ package day17;
 
 public enum ChessPiece {
     KING_WHITE(100, "♔"), QUEEN_WHITE(9, "♕"), ROOK_WHITE(5, "♖"),
-    BISHOOP_WHITE(3, "♗"), KNIGHT_WHITE(3, "♘"), PAWN_WHITE(1, "♙"),
+    BISHOOP_WHITE(3.5, "♗"), KNIGHT_WHITE(3, "♘"), PAWN_WHITE(1, "♙"),
     KING_BLACK(100, "♚"), QUEEN_BLACK(9, "♛"), ROOK_BLACK(5, "♜"),
-    BISHOOP_BLACK(3, "♝"), KNIGHT_BLACK(3, "♞"), PAWN_BLACK(1, "♟"),
+    BISHOOP_BLACK(3.5, "♝"), KNIGHT_BLACK(3, "♞"), PAWN_BLACK(1, "♟"),
     EMPTY(-1, "_");
 
-    private int rate;
+    private double rate;
     private String symbol;
 
-    ChessPiece(int rate, String symbol) {
+    ChessPiece(double rate, String symbol) {
         this.rate = rate;
         this.symbol = symbol;
     }
 
-    public int getRate() {
+    public double getRate() {
         return rate;
     }
 
--- src/main/java/day17/Task2.java
+++ src/main/java/day17/Task2.java
@@ -1,18 +1,24 @@
 package day17;
 
+import java.util.Scanner;
+
 public class Task2 {
     public static void main(String[] args) {
-        ChessPiece empty = ChessPiece.EMPTY;
-        ChessPiece[][] chessBoard = {
-                {ChessPiece.ROOK_BLACK, empty, empty, empty, empty, ChessPiece.ROOK_BLACK, ChessPiece.KING_BLACK, empty},
-                {empty, ChessPiece.ROOK_WHITE, empty, empty, ChessPiece.PAWN_BLACK, ChessPiece.PAWN_BLACK, empty, ChessPiece.PAWN_BLACK},
-                {ChessPiece.PAWN_BLACK, empty, ChessPiece.KNIGHT_BLACK, empty, empty, empty, ChessPiece.PAWN_BLACK, empty},
-                {ChessPiece.QUEEN_BLACK, empty, empty, ChessPiece.BISHOOP_WHITE, empty, empty, empty, empty},
-                {empty, empty, empty, ChessPiece.BISHOOP_BLACK, ChessPiece.PAWN_WHITE, empty, empty, empty},
-                {empty, empty, empty, empty, ChessPiece.BISHOOP_WHITE, ChessPiece.PAWN_WHITE, empty, empty},
-                {ChessPiece.PAWN_WHITE, empty, empty, ChessPiece.QUEEN_WHITE, empty, ChessPiece.PAWN_WHITE, empty, ChessPiece.PAWN_WHITE},
-                {empty, empty, empty, empty, empty, ChessPiece.ROOK_WHITE, ChessPiece.KING_WHITE, empty}
-        };
+        //Вводим константу - размер доски
+        final int boardSize = 8;
+
+        //Создаем доску
+        ChessPiece[][] chessBoard = new ChessPiece[boardSize][boardSize];
+        //Заполняем доску с помощью цикла пустыми значениями
+        for (int i = 0; i < boardSize; i++) {
+            for(int j = 0; j < boardSize; j++) {
+                chessBoard[i][j] = ChessPiece.EMPTY;
+            }
+        }
+        //Расставляем фигуры:
+        chessBoard[0][0] = ChessPiece.ROOK_BLACK;
+        chessBoard[0][5] = ChessPiece.ROOK_BLACK;
+        chessBoard[0][6] = ChessPiece.KING_BLACK;
 
         ChessBoard chess = new ChessBoard(chessBoard);
         chess.print();
